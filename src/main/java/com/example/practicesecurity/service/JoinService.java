d4be5fff71e63a173ba262bbdd0477adef4eb530
eunbb <fakesilverpark@gmail.com>
2025-02-14 21:48:20 +0900

    250214 : JoinService 구현

--- src/main/java/com/example/practicesecurity/service/JoinService.java
+++ src/main/java/com/example/practicesecurity/service/JoinService.java
@@ -1,11 +1,25 @@
 package com.example.practicesecurity.service;
 
 import com.example.practicesecurity.dto.JoinDTO;
+import com.example.practicesecurity.entity.UserEntity;
+import com.example.practicesecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class JoinService {
 
+    private final UserRepository userRepository;
+
     public void joinProcess(JoinDTO joinDTO){
+
+        UserEntity data = new UserEntity();
+
+        data.setUsername(joinDTO.getUsername());
+        data.setPassword(joinDTO.getPassword());
+        data.setRole("ROLE_USER");
+
+        userRepository.save(data);
     }
 }
